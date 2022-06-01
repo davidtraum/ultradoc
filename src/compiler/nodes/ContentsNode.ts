3d8a35630fd5a67534090fea4e99a23c99245f2c
David Traum <david@traum.me>
2022-06-01 20:00:17 +0200

    Initial

--- src/compiler/nodes/ContentsNode.ts
+++ src/compiler/nodes/ContentsNode.ts
@@ -31,7 +31,7 @@ export class ContentsNode extends DocumentNode {
         this.toc = '';
         for(const entry of compiler.bodyContent) {
             if(entry instanceof TextNode) {
-                if(entry.args.classes.includes('headline') || entry.args.classes.includes('large') && entry.args.content.length > 0) {
+                if(entry.args.classes.includes('headline') || entry.args.classes.includes('large') && !entry.args.attributes['no-toc'] && entry.args.content.length > 0) {
                     this.toc += `<div class="toc-entry">${entry.args.content}</div>`;
                 }
             }
